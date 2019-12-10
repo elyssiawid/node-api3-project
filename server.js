1caa6008a572e6e3d64cb236179f390a66789deb
Elyssia Widjaja <Elyssia@elyssias-mbp.lan>
2019-12-10 14:44:56 -0800

    mvp

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `${req.method} ${req.originalUrl} at ${new Date().toISOString()}`
+  );
+  next();
+}
+
+server.use(logger);
+server.use("./api/user", userRouter);
+server.use("./api/post", postRouter);
 
 module.exports = server;
